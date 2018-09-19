1704294e0d508f32bd8207550444b98bb48f3cc9
rxroutsong@gmail.com <troy@nfl.com>
2018-09-19 16:12:54 -0700

    fixed variable name issue with lib/dna import

--- lib/dna.py
+++ lib/dna.py
@@ -1,31 +1,57 @@
 from itertools import groupby
 
-def revcomp(dna):
+def countnuc(strand):
+    anuc, tnuc, cnuc, gnuc, unuc = 0, 0, 0, 0, 0
+    strand = strand.upper()
+
+    for c in strand:
+        if c == "A":
+            anuc += 1
+        if c == "T":
+            tnuc += 1
+        if c == "C":
+            cnuc += 1
+        if c == "G":
+            gnuc += 1
+        if c == "U":
+            unuc += 1
+
+    # account for RNA strand as well
+    if(tnuc == 0):
+        tu_nuc = unuc
+    else:
+        tu_nuc = tnuc
+
+    counts = str(anuc) + " " + str(cnuc) + " " + str(gnuc) + " " + str(tu_nuc)
+
+    return counts
+
+def revcomp(strand):
     nuc = {
         'A': 'T',
         'T': 'A',
         'C': 'G',
         'G': 'C'
     }
-    
+
     reverse = ''
-    
-    for char in dna:
+
+    for char in strand:
         reverse += nuc[char]
-        
+
     return reverse[::-1]
-    
+
 def gc_content(dna):
     gsandcs = 0.0
-    
+
     for char in dna:
         if char == 'G' or char == 'C':
             gsandcs+=1
-    
+
     gc_per = ((gsandcs/len(dna))*100)
-    
+
     return gc_per
-    
+
 def fasta_parse(fasta_data):
     # input should be a local file location
     fo = open(fasta_data)
@@ -36,36 +62,36 @@ def fasta_parse(fasta_data):
         # join all sequence lines to one.
         seq = "".join(s.strip() for s in faiter.next())
         yield header, seq
-        
+
 def fib_num(n, k):
     ## Fibonacci rabbits sequence,
     ## where n = numbers of months,
     ## and k = liters per pair of breeding rabbits
-    
+
     a, b = 1, 1
     for _ in xrange(n):
-        yield a 
+        yield a
         a, b = b, (k*a) + b
-        
+
 def hamngdist(s,t):
     ## Calculate the hamming distance between two strings:
     ##
     ## dh(s1,s1) = number of differing nucleotides between two strands
     ##             of the same length
     ##
-    
+
     if(len(s) != len(t)):
         raise Exception('The two strands not the same length.')
-        
+
     h, i = 0, 0
-    
+
     for nuc in s:
         if(s[i] != t[i]):
             h+=1
         i+=1
-    
+
     return h
-    
+
 def prob_dom_pheno(k,m,n):
     ##
     ## Calculate the probability of two randomly selected mating individuals will
@@ -76,29 +102,29 @@ def prob_dom_pheno(k,m,n):
     ## m - heterozygous individuals within a population
     ## n - homozygous recessive individuals within popluation
     ##
-    
+
     ## total population
-    
-    t = k + m + n 
-    
+
+    t = k + m + n
+
     ##
     ## Probability of selecting mating pairs
-    ## 
-    
+    ##
+
     ##
     ## Selection of first mating organism
     ##
-    
-    k-odds, m-odds, n-odds = (k/t), (m/t), (n/t)
-    
+
+    k_odds, m_odds, n_odds = (k/t), (m/t), (n/t)
+
     ##
     ## Selection of second mating organism
     ##
-    
-    kk-odds, km-odds, kn-odds = (k-1)/(t-1), (m)/(t-1), (n)/(t-1)
-    mm-odds, mk-odds, mn-odds = (m-1)/(t-1), (k)/(t-1), (n)/(t-1)
-    nn-odds, nk-odds, nm-odds = (n-1)/(t-1), (k)/(t-1), (n)/(t-1)
-    
+
+    kk_odds, km_odds, kn_odds = (k-1)/(t-1), (m)/(t-1), (n)/(t-1)
+    mm_odds, mk_odds, mn_odds = (m-1)/(t-1), (k)/(t-1), (n)/(t-1)
+    nn_odds, nk_odds, nm_odds = (n-1)/(t-1), (k)/(t-1), (n)/(t-1)
+
     ##
     ## Punnett square odds for dominant phenotype
     ##
@@ -108,5 +134,5 @@ def prob_dom_pheno(k,m,n):
     ##      dom-recessive = MM x mm = (4/4)
     ##    hetero-homo-dom = Mm x MM = (4/4)
     ##    hetero-homo-rec = Mm x mm = (1/2)
-    
-    two-homo-dominant, two-heterozygous, two-homo-recessive, dom-recessive, hetero-homo-dom, hetero-homo-rec = 1.00, 0.75, 0.00, 1.00, 1.00, 0.50
+
+    two_homo_dominant, two_heterozygous, two_homo_recessive, dom_recessive, hetero_homo_dom, hetero_homo_rec = 1.00, 0.75, 0.00, 1.00, 1.00, 0.50
