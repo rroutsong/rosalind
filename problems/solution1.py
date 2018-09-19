d0e59e7d1a801728ab924a710f53bc988a7fa6a0
rxroutsong@gmail.com <troy@nfl.com>
2018-09-19 16:18:50 -0700

    fixed variable name issue with lib/dna import

--- problems/solution1.py
+++ problems/solution1.py
@@ -1,1 +1,6 @@
-require '../lib/dna'
+# insert rosalind problem libs into system path
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '../lib'))
+
+# import the rosalind dna library
+import dna
